8d5ad339952c403bcf51bd0c1e33e8241b50866d
Jose A. Raposo <joseantonio.raposo.perez@iesdonana.org>
2024-05-09 21:33:51 +0200

    relaciones y modelos

---
+++ app/Http/Controllers/ArtistaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreArtistaRequest;
+use App\Http\Requests\UpdateArtistaRequest;
+use App\Models\Artista;
+
+class ArtistaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreArtistaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Artista $artista)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Artista $artista)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateArtistaRequest $request, Artista $artista)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Artista $artista)
+    {
+        //
+    }
+}
